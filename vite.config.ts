2ed92d2828f23a8c4139c02a9727141b4152d34d
varditomer <tomer.vardi@gmail.com>
2024-05-25 16:51:02 +0300

    update to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bulbabook_front/', // Replace '<repository-name>' with your repo name
 })
